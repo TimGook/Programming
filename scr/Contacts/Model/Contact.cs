46ef88feabaad765088f81840235dad9956b762f
TimGook <timofej.vetrov.04@mail.ru>
2024-03-14 16:27:13 +0700

    Added

---
+++ scr/Contacts/Model/Contact.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Model
+{
+    /// <summary>
+    /// Описывает данные контакта.
+    /// </summary>
+    internal class Contact
+    {
+        /// <summary>
+        /// Имя контакта.
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Номер телефона контакта.
+        /// </summary>
+        private string _phone;
+
+        /// <summary>
+        /// Электронная почта контакта.
+        /// </summary>
+        private string _email;
+
+        /// <summary>
+        /// Задаёт и возвращает имя контакта.
+        /// </summary>
+        public string Name 
+        { 
+            get 
+            { 
+                return _name; 
+            } 
+            set 
+            { 
+                _name = value; 
+            }
+        }
+
+        /// <summary>
+        /// Задаёт и возвращает номер телефона контакта.
+        /// </summary>
+        public string Phone
+        {
+            get
+            {
+                return _phone;
+            }
+            set
+            {
+                _phone = value;
+            }
+        }
+
+        /// <summary>
+        /// Задаёт и возвращает электронную почту контакта.
+        /// </summary>
+        public string Email 
+        {
+            get
+            {
+                return _email;
+            }
+            set 
+            { 
+                _email = value; 
+            }
+        }
+
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="name">Имя контакта.</param>
+        /// <param name="phone">Номер телефона контакта.</param>
+        /// <param name="email">Электронная почта контакта.</param>
+        public Contact(string name, string phone, string email)
+        {
+            Name = name;
+            Phone = phone;
+            Email = email;
+        }
+    }
+}
